2fd22f743509fc274a04146b4a32b1d05ae97d97
rajendra prasad <it.rajendra@gmail.com>
2015-12-02 12:07:13 -0800

    Units link added

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var units = require('./routes/units');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,8 +22,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/units', units);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
