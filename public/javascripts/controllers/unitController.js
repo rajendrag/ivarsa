547f49d53eac79480e292acf723a40888f0c780d
RP <it.rajendra@gmail.com>
2015-12-06 00:17:07 -0800

    - Angular routes added - template are being used

--- public/javascripts/controllers/unitController.js
+++ public/javascripts/controllers/unitController.js
@@ -1,7 +1,6 @@
-angular.module('unitController', [])
-
+var unitControllers = angular.module('unitControllers', []);
 	// inject the Unit service factory into our controller
-	.controller('unitController', ['$scope', 'unitService', function($scope, unitService) {
+	unitControllers.controller('unitListCtrl', ['$scope', '$location', 'unitService', '$routeParams', function($scope, $location, unitService, $routeParams) {
 		$scope.formData = {};
 		$scope.loading = true;
 
@@ -14,5 +13,14 @@ angular.module('unitController', [])
 				$scope.loading = false;
 			});
 
+		$scope.viewDetails = function(unit) {
+		   //$location.path = "#/units/"+unit.iq_unit_mstr_id;
+		    window.location = "#/units/"+unit.iq_unit_mstr_id;
+		}
 
 	}]);
+
+	unitControllers.controller('unitDetailCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
+	    $scope.unitId = $routeParams.unitId;
+	}]);
+
--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,1 +1,16 @@
-angular.module('ivarsa', ['unitController', 'unitService']);
+var ivarsaApp = angular.module('ivarsa', ['ngRoute', 'unitControllers', 'unitService']);
+ivarsaApp.config(['$routeProvider',
+                   function($routeProvider) {
+                     $routeProvider.
+                       when('/units', {
+                         templateUrl: 'angviews/unit-list.html',
+                         controller: 'unitListCtrl'
+                       }).
+                       when('/units/:unitId', {
+                         templateUrl: 'angviews/unit-details.html',
+                         controller: 'unitDetailCtrl'
+                       }).
+                       otherwise({
+                         redirectTo: '/units'
+                       });
+                   }]);
