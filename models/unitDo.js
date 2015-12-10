0335cfddfb5f0f53a7daef4139acacfe8507a511
RP <it.rajendra@gmail.com>
2015-12-09 22:34:52 -0800

    -JS code formatting

--- models/unitDo.js
+++ models/unitDo.js
@@ -1,54 +1,55 @@
 "use strict";
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define("Unit", {
-                                      id: {
-                                              type: DataTypes.STRING,
-                                              field: 'iq_unit_mstr_id',
-                                              primaryKey : true
-                                      },
-                                      unitName: {
-                                            type: DataTypes.STRING,
-                                            field: 'unit_name'
-                                      },
-                                     owner: {
-                                             type: DataTypes.STRING,
-                                             field: 'owner'
-                                     },
-                                      description: {
-                                              type: DataTypes.STRING,
-                                              field: 'description'
-                                      },
-                                      createdBy: {
-                                                type: DataTypes.STRING,
-                                                field: 'created_by'
-                                        },
-                                      modifiedBy: {
-                                               type: DataTypes.STRING,
-                                               field: 'modified_by'
-                                       },
-                                      nofChairs: {
-                                                type: DataTypes.INTEGER,
-                                                field: 'nof_chairs'
-                                      },
-                                      nofBeds: {
-                                                type: DataTypes.INTEGER,
-                                                field: 'nof_beds'
-                                      }
+  return sequelize.define("Unit", {
+    id: {
+      type: DataTypes.STRING,
+      field: 'iq_unit_mstr_id',
+      primaryKey: true
+    },
+    unitName: {
+      type: DataTypes.STRING,
+      field: 'unit_name'
+    },
+    owner: {
+      type: DataTypes.STRING,
+      field: 'owner'
+    },
+    description: {
+      type: DataTypes.STRING,
+      field: 'description'
+    },
+    createdBy: {
+      type: DataTypes.STRING,
+      field: 'created_by'
+    },
+    modifiedBy: {
+      type: DataTypes.STRING,
+      field: 'modified_by'
+    },
+    nofChairs: {
+      type: DataTypes.INTEGER,
+      field: 'nof_chairs'
+    },
+    nofBeds: {
+      type: DataTypes.INTEGER,
+      field: 'nof_beds'
+    }
 
-                                    },
-                                    {
-                                        timestamps: true,
-                                        createdAt: 'created_datetime',
-                                        updatedAt: 'modified_datetime',
-                                        deletedAt: false,
-                                        force:false,
-                                        freezeTableName: true, // Model tableName will be the same as the model name
-                                        tableName: 'iq_unit_mstr',
-                                        classMethods: {
-                                              associate: function(models) {
-                                                models.Unit.hasMany(models.UnitOpHours, {foreignKey: "iq_unit_mstr_id"})
-                                              }
-                                        }
-                                    });
+  }, {
+    timestamps: true,
+    createdAt: 'created_datetime',
+    updatedAt: 'modified_datetime',
+    deletedAt: false,
+    force: false,
+    freezeTableName: true, // Model tableName will be the same as the model name
+    tableName: 'iq_unit_mstr',
+    classMethods: {
+      associate: function(models) {
+        models.Unit.hasMany(models.UnitOpHours, {
+          foreignKey: "iq_unit_mstr_id"
+        })
+      }
+    }
+  });
 }
--- routes/units.js
+++ routes/units.js
@@ -1,28 +1,33 @@
 var express = require('express');
 var router = express.Router();
 //importing models
-var models  = require('../models');
+var models = require('../models');
 var UnitDAO = models.Unit;
 
 
 /* GET units listing. */
 router.get('/', function(req, res, next) {
-     UnitDAO.findAll().then(function(units) {
-      res.send(units);
-    }).catch(function(err){
-        console.log(err);
-        res.send('Error!!');
-    });
+  UnitDAO.findAll().then(function(units) {
+    res.send(units);
+  }).catch(function(err) {
+    console.log(err);
+    res.send('Error!!');
+  });
 });
 
 router.get('/:unitId', function(req, res, next) {
 
-    UnitDAO.findOne({ where: {id: req.params.unitId}, include: [ models.UnitOpHours ] }).then(function(unit) {
-      res.send(unit);
-    }).catch(function(err){
-        console.log(err);
-        res.send('Error!!');
-    });
+  UnitDAO.findOne({
+    where: {
+      id: req.params.unitId
+    },
+    include: [models.UnitOpHours]
+  }).then(function(unit) {
+    res.send(unit);
+  }).catch(function(err) {
+    console.log(err);
+    res.send('Error!!');
+  });
 });
 
 module.exports = router;
